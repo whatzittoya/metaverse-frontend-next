4eb0ad2727d840b8e99305c0424a00550b538d59
whatzittoya <whosendall@gmail.com>
2022-11-07 07:53:37 +0700

    getobject

---
+++ pages/api/render/getobject/[...id].tsx
@@ -0,0 +1,19 @@
+const api = process.env.NEXT_PUBLIC_API;
+const token = process.env.NEXT_PUBLIC_TOKEN;
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const { id } = req.query;
+    if (id !== null) {
+      fetch(`${api}items/object/`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          res.status(200).json(JSON.parse(data.data));
+        });
+    }
+  }
+}
