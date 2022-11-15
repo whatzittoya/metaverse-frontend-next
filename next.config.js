00149622d532fdc8223307137342cb9150413660
whatzittoya <whosendall.com>
2022-11-15 21:03:06 +0700

    cors

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ];
+  },
+};
