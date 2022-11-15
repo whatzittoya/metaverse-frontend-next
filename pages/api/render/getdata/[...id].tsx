5abe171960af9bafcf29ddead3f32d1212b9ab0e
whatzittoya <whosendall.com>
2022-11-15 22:19:38 +0700

    rest cors

--- pages/api/render/getdata/[...id].tsx
+++ pages/api/render/getdata/[...id].tsx
@@ -1,4 +1,12 @@
+import NextCors from "nextjs-cors";
+
 export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   if (req.method === "GET") {
     const data = [
       {
--- pages/api/render/getscene/[...id].ts
+++ pages/api/render/getscene/[...id].ts
@@ -1,6 +1,14 @@
+import NextCors from "nextjs-cors";
+
 const api = process.env.NEXT_PUBLIC_API_LOCAL;
 const token = process.env.NEXT_PUBLIC_TOKEN;
 export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   if (req.method === "GET") {
     const { id } = req.query;
     if (id !== null) {
